c80ba65d37e773157de735b3598ae7c8315f76a6
br1 <delgado.hebru24@triana.salesianos.edu>
2024-11-25 10:57:57 +0100

    aaaaaaaaaaa

--- src/app/pages/fav-list/fav-list.component.ts
+++ src/app/pages/fav-list/fav-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FavoritesService } from '../../services/favorites.service';
+import { FavMovieResponse } from '../../models/fav-movie-response';
 
 @Component({
   selector: 'app-fav-list',
@@ -7,9 +8,20 @@ import { FavoritesService } from '../../services/favorites.service';
   styleUrl: './fav-list.component.css'
 })
 export class FavListComponent implements OnInit {
+
+  favoriteMovies : FavMovieResponse[] = [];
+
   constructor(private favService : FavoritesService) {}
   
   ngOnInit() {
+  }
+
+  getFavoriteMovies() {
+    this.favService.getFavoriteMovies().subscribe(
+      (data) => {
+        console.log(data);
+      }
+    );
   }
 
 
--- src/app/services/favorites.service.ts
+++ src/app/services/favorites.service.ts
@@ -2,10 +2,11 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AccountDetailsResponse } from '../models/account-details.interface';
 import { Observable } from 'rxjs';
+import { FavMovieResponse } from '../models/fav-movie-response';
 
 
 const API_KEY = "de28babb0baeed53e1255cd2b2bd2e15";
-const API_BASE_URL = "https://api.themoviedb.org/3/account/{account_id}/favorite";
+const API_BASE_URL = "https://api.themoviedb.org/3/account/21623231/favorite";
 
 @Injectable({
   providedIn: 'root'
@@ -16,12 +17,14 @@ export class FavoritesService {
 
 
 
-  getFavoriteMovies(): Observable<AccountDetailsResponse> {
+  getFavoriteMovies(): Observable<FavMovieResponse> {
     let sessionId = localStorage.getItem('session_id');
-    return this.http.get<AccountDetailsResponse>(
-      `${API_BASE_URL}/movies?api_key=${API_KEY}&session_id=${sessionId}`
+    return this.http.get<FavMovieResponse>(
+
+      `${API_BASE_URL}/movie?session_id=${sessionId}&api_key=${API_KEY}`
     );
   }
+
 
 
   
